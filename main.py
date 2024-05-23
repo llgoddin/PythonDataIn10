2891f11f9e6f8511be53680036a95f5b29cee1fb
Lucas <llgoddin@gmail.com>
2024-05-23 15:57:13 -0400

    init

---
+++ main.py
@@ -0,0 +1,29 @@
+#integers
+x = 5
+
+#floats
+y = 5.0
+
+#doubles
+a = 5.0
+
+#strings
+z = "Hello, World!"
+
+#characters
+c = 'c'
+
+#booleans
+b = True
+
+#arrays
+arr = [1, 2, 3, 4, 5]
+
+#lists
+lst = [1, 2, 3, 4, 5]
+
+lst.append(6)
+popped_value = lst.pop()
+
+print(lst)
+print("Popped Value: ", popped_value)
